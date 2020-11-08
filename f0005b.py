69bf14b211bfaf33f1f4868c54e469b8acc53da2
preinerdom2005 <72602590+preinerdom2005@users.noreply.github.com>
2020-11-08 18:55:04 +0100

    Add files via upload

---
+++ f0005b.py
@@ -0,0 +1,9 @@
+jelszó=('pornhub')
+tipp=input('mi a jelszó?')
+if tipp == jelszó: 
+    print('Eltaláltad!')
+    print('ügyes vagy!')
+else: 
+    print('Ez most sajnos nem sikerült...')
+    print('De majd legközelebb... talán... ha nagyobb szerencséd lesz...')
+print('Pápá!')
---
+++ f0005c.py
@@ -0,0 +1,6 @@
+szám=input('mondj egy számot!')
+szám=int(szám)
+if szám>=0:
+    print('a',szám,'természetes szám.')
+else:
+    print('a',szám,'egy negatív egész szám')
